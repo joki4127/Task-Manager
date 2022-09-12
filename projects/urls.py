c96a95bfd93d19db30b7a9759b79d534c411e0c4
Woojae <joki4127@gmail.com>
2022-09-12 14:14:18 -0600

    Feature 5 complete

---
+++ projects/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from projects.views import (
+    ProjectListview,
+)
+
+urlpatterns = [
+    path("", ProjectListview.as_view(), name="list_projects"),
+]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from projects.models import Project
+
 
 # Create your views here.
+class ProjectListview(ListView):
+    model = Project
+    template_name = "projects/list.html"
+    context_object_name = "projectlist"
