0e5f006adfd4367e0022b7301203f4ab0408d367
Woojae <joki4127@gmail.com>
2022-09-13 12:01:42 -0600

    Feature 17 complete

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from tasks.views import (
     TaskCreateview,
     TaskListview,
+    TaskUpdateview,
 )
 
 
 urlpatterns = [
     path("create/", TaskCreateview.as_view(), name="create_task"),
     path("mine/", TaskListview.as_view(), name="show_my_tasks"),
+    path("<int:pk>/complete/", TaskUpdateview.as_view(), name="complete_task"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
 
 from tasks.models import Task
 
@@ -30,3 +31,11 @@ class TaskListview(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Task.objects.filter(assignee=self.request.user)
+
+
+class TaskUpdateview(LoginRequiredMixin, UpdateView):
+    model = Task
+    template_name = "tasks/update.html"
+    fields = ["is_completed"]
+    context_object_name = "taskupdate"
+    success_url = reverse_lazy("show_my_tasks")
